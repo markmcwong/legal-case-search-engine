a26e5fec1a147349cfdc77e53d41b590c80ff6b8
Zhang-Yunjie <e0421414@u.nus.edu>
2022-04-19 23:09:34 +0800

    Update to latest version and update punctuation removal

--- index.py
+++ index.py
@@ -1,99 +1,245 @@
 #!/usr/bin/python3
 import re
+#import requests
 import nltk
 import sys
 import getopt
 import csv
+import string
+import time
 from index_helper import *
-from setup import setup_dependencies
+from translator import britishize
+from profiler import profile
+from tqdm import tqdm
+
+# Turn on/off compression methods
+DELTA_COMPRESSION = True
+VB_COMPRESSION = True
+
 
 def usage():
     print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
 
-def build_index(in_file, out_dict, out_postings, out_docLengths = "docLengthsFile.txt"):
+@profile
+def build_index(in_file, out_dict, out_postings):
     """
     build index from documents stored in the input file,
     then output the dictionary file and postings file
     """
+    global visited_id
+    global N
+    # Cache stemmed words to optimise stemming
+    stemDict = {}
+    # Creater stemmer
+    stemmer = nltk.stem.PorterStemmer()
+
     print('indexing...')
-    # This is an empty method
-    # Pls implement your code in below
 
     # Expand field size limit as some columns in csv have very large fields
-    field_size_limit = sys.maxsize
+    field_size_limit = sys.maxsize    
     while True:
         try:
             csv.field_size_limit(field_size_limit)
             break
         except OverflowError:
             field_size_limit = int(field_size_limit / 10)
-
+        
     # Read contents from csv file
     doc_words = []
-    with open(in_file, encoding="utf-8") as csv_file:
+    
+    # Store visited docID to skip duplicate doc
+    visited_id = {}
+    
+    with open(in_file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter = ',')
-        line_count = 0
+        header = next(csv_reader) # get the first row (table header)
+        #print(f'Column names: {", ".join(header)}') ## document_id, title, content, date_posted, court
+        #print(header)
+        
         for row in csv_reader:
-            if line_count == 0:
-                print(f'Column names: {", ".join(row)}') ## document_id, title, content, date_posted, court
-                line_count += 1
-            else:
-                print(row[0])
-                doc_words.append((row[0], (row[1] + ' ' + row[2] + ' ' + row[4])[:200])) # Limit for testing to 200 characters
-
-    print("End of file")
-
-    # store token-docId pairs
-    token_stream = []
-
-    # store length of documents
-    docLengths = []
-
+            doc_id = row[0]
+            if doc_id in visited_id:
+                court_list = visited_id[doc_id]
+                court_list.append(row[4])
+                continue
+                
+            visited_id[doc_id] = [row[4]]
+            #print(row[0]) ## print document ID
+            doc_words.append((row[0], (row[1] + ' ' + row[2] + ' ' + row[4]))) #Combine title, content,court, Limit for testing to 200 characters
+    
+    print("End of file parsing")
+    # testing for duplicate doc ID
+    #print(visited_id["247336"])
+    #print(visited_id["3926753"])
+    
+    
+    # Get total number of documents
+    N = len(doc_words)
+    #print(N)
+    
+    # Initialize dictionary
+    dictionary = {}
+    dictionary['DOC_LENGTH'] = {}
+    
+    # debugging
+    #doc_words = doc_words[3000:]
+    
     BREAKNUM = 1
-    for doc in doc_words:
-        # Do preprocessing on documents
-        cleaned_tokens = clean_tokens(tokenize_text(doc[1]), remove_punc = True)
-        # Save as term-docid-pos tuples
-        new_tokens = [(term, doc[0], pos) for pos, term in enumerate(cleaned_tokens)]
-        # Calculate document length
-        docLengths.append((doc[0], get_doc_length(cleaned_tokens)))
-        token_stream += new_tokens
-        if BREAKNUM == 2:
+    for doc in tqdm(doc_words):
+        if BREAKNUM == 1000:
             break # for testing purposes, check 2 doc
         BREAKNUM += 1
-
-    # Save document lengths to file (to be used during searching)
-    if os.path.exists(out_docLengths):
-        os.remove(out_docLengths)
-    ## <<<< currently doclengths not sorted, comes as list of tuples randomly arranged [(docid, length)...]
-    with open(out_docLengths, "ab") as outdl_f:
-        pickle.dump(docLengths, outdl_f)
-
-    # Invert to dictionary
-    # First, sort token_stream by term, docId, pos
-    token_stream.sort()
-
-    # Create dictionary
-    freq_dic = {}
-
-    for token in token_stream:
-        term = token[0]
-        docId = token[1]
-        pos = token[2]
-        # store in frequency dictionary
-        ## for new terms, record first docFreq and first termFreq and its position
-        if term not in freq_dic:
-            freq_dic[term] = {docId: (1, [pos])}
-        else:
-            ## For existing term, append docId and pos if from different document, add term frequency and pos otherwise
-            if docId not in freq_dic[term]:
-                freq_dic[term][docId] = (1, [pos])
+        """
+        if BREAKNUM % 100 == 0:
+            print("Current progress:", BREAKNUM)
+            print("--- %s seconds ---" % (time.time() - start))
+        """
+        # Apply case folding, converting text to lower case
+        text = doc[1].lower()
+        
+        # Remove non-latin characters
+        regex = re.compile('[^\u0020-\u024F]')
+        text = regex.sub('',text)
+        
+        # Standardize to british english
+        text = britishize(text)
+        
+        # Remove single quotations
+        text = text.replace("'","")
+        
+        # Remove all punctuations
+        text = text.translate(str.maketrans('', '', string.punctuation))
+        
+        # Initialize term frequency map to store term frequency and a list of positions
+        freq_map = {}
+        
+        # Initialize term position
+        pos = 0
+        
+        # Apply tokenization and stemming
+        for sentence in nltk.sent_tokenize(text):
+            for word in nltk.word_tokenize(sentence):
+                # Skip term that are solely punctuations
+                #if all(char in string.punctuation for char in word):
+                #    continue
+                    
+                if word in stemDict:
+                    stemmed_word = stemDict[word]
+                else:
+                    # Apply stemming
+                    stemmed_word = stemmer.stem(word)
+                    stemDict[word] = stemmed_word
+                
+                # Update term frequency map
+                if stemmed_word not in freq_map:
+                    freq_map[stemmed_word] = [1,[pos]]
+                else:
+                    freq_map[stemmed_word][0] += 1
+                    freq_map[stemmed_word][1].append(pos)
+                
+                # Update position
+                pos += 1
+        
+        # Initialise document length for this document
+        doc_length = 0
+        
+        # Update document frequency and posting list
+        for term, list in freq_map.items():
+            tf = list[0]
+            pos_list = list[1]
+            
+            # Apply log-frequency weighting scheme
+            log_tf = 1 + math.log(tf,10)
+            # Update document length
+            doc_length += log_tf ** 2
+            
+            if DELTA_COMPRESSION:
+                # Compress position list using delta representation
+                pos_list = calculate_deltas(pos_list)
+            
+            # structure of each value in the dictionary:
+            # (document_frequency, [(docID_1, log_tf_1, [position1,position2...]), (docID_2, log_tf_2, [position1,position2...]) ... (docID_n, log_tf_n, [position1,position2...])])
+            
+            if term not in dictionary:
+                dictionary[term] = (1, [(int(doc[0]), log_tf, pos_list)])
             else:
-                freq_dic[term][docId] = (freq_dic[term][docId][0] + 1, freq_dic[term][docId][1] + [pos])
+                # Update current document frequency
+                df = dictionary[term][0] + 1
+                posting = dictionary[term][1] + [(int(doc[0]), log_tf, pos_list)]
+                dictionary[term] = (df,posting)
+        
+        # Compute and store the document length for current document, to be used in normalization in searching
+        dictionary['DOC_LENGTH'][int(doc[0])] = math.sqrt(doc_length)
+    
+    invert(dictionary,out_dict, out_postings)
+    
+def invert(dictionary,out_dict, out_postings):
+    global visited_id
+    
+    print("Inverting...")
+    
+    f_dict = open(out_dict, "wb")
+    f_post = open(out_postings, 'wb')
+    # erase any existing file
+    f_dict.truncate(0)
+    f_post.truncate(0)
+    
+    # Write dictionaries and postings to dictionary.txt and posting.txt
+    for term, value in tqdm(dictionary.items()):
+        #print("term: ",term)
+        #print("value:", value)
+        
+        if term == "DOC_LENGTH":
+            pointer = f_post.tell()
+            pickle.dump(value, f_post)
+            dictionary[term] = pointer
+            continue
+        
+        # Store the posting lists to disk
+        df,posting = value
+        pointer = f_post.tell()
+        
+        # Posting format: [(docID_1, log_tf_1, [position1,position2...]), (docID_2, log_tf_2, [position1,position2...]) ... (docID_n, log_tf_n, [position1,position2...])])
+        if VB_COMPRESSION:
+            # Apply variable byte encoding to posting
+            encoded_posting = []
+            for tuple in posting:
+                docID = vbcode.VBEncodeNumber(int(tuple[0]))
+                log_tf = vbcode.VBEncodeNumber(int(tuple[1]))
+                pos_list = vbcode.VBEncode(tuple[2])
+                encoded_posting.append((docID,log_tf,pos_list))
+            
+            pickle.dump(encoded_posting,f_post)
+            
+        else:
+            pickle.dump(posting, f_post)
+        
+        # Convert df to idf
+        global N
+        idf = math.log((N / df),10)
+        
+        # Update dictionary with idf and pointer
+        dictionary[term] = (idf,pointer)
+    
+    # Write the mapping between document id and a list of court names into posting.txt using the special key "DOC_COURT"
+    pointer = f_post.tell()
+    pickle.dump(visited_id, f_post)
+    dictionary["DOC_COURT"] = pointer
+    
+    # Store the dictionary to disk
+    pickle.dump(dictionary,f_dict)
+    f_dict.close()
+    f_post.close()
+    print("Finished writing")
+    
+def calculate_deltas(numbers):
+    if not numbers:
+        return numbers
 
-    # Create dictionary and posting list
-    invert(freq_dic, out_dict, out_postings)
-
+    deltas = [numbers[0]]
+    for i, n in enumerate(numbers[1:]):
+        deltas.append(n - numbers[i])
+    return deltas
 
 input_file = output_file_dictionary = output_file_postings = None
 
@@ -117,5 +263,6 @@ if input_file == None or output_file_postings == None or output_file_dictionary 
     usage()
     sys.exit(2)
 
-setup_dependencies()
+start = time.time()
 build_index(input_file, output_file_dictionary, output_file_postings)
+print("--- %s seconds ---" % (time.time() - start))
